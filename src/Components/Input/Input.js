2db7701f7b915967dd1894188798fddefd62d655
Mark MacHarris <mark@leadingreach.com>
2022-07-01 15:48:13 -0600

    remove setOpen(false) from input

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -15,7 +15,6 @@ const Input = ({
 
   const handleInput = ({ target: { value } }) => {
     setTimeValue(value);
-    if (value.match(/((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/)) setOpen(false);
   };
 
   const handleClick = () =>
